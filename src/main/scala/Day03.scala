930e5355b973d67da726fa61f0b9a42b9ccbca90
Miskpec <info@mikspec.pl>
2020-01-12 22:35:00 +0100

    Day 03 part 2

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -14,8 +14,8 @@ object Day03 extends App {
                   )
 
   @tailrec
-  def processElement(direction: String, dist: Int, x: Int, y: Int, index: Int, plan: Map[(Int, Int, Int), Int]): (Map[(Int, Int, Int), Int], Int, Int) = {
-    if (dist == 0) (plan, x, y)
+  def processElement(direction: String, dist: Int, x: Int, y: Int, index: Int, plan: Map[(Int, Int, Int), Int], wireLength: Int): (Map[(Int, Int, Int), Int], Int, Int, Int) = {
+    if (dist == 0) (plan, x, y, wireLength)
     else {
       var (xNew, yNew) = (x, y)
       direction match {
@@ -24,24 +24,24 @@ object Day03 extends App {
         case "U" => yNew -= 1
         case "D" => yNew += 1
       }
-      processElement(direction, dist - 1, xNew, yNew, index, plan + ((xNew, yNew, index) -> (plan.getOrElse((xNew, yNew, index), 0) + 1)))
+      val length = plan.getOrElse((xNew, yNew, index), wireLength + 1)
+      processElement(direction, dist - 1, xNew, yNew, index, plan + ((xNew, yNew, index) -> length), wireLength + 1)
     }
   }
 
   @tailrec
-  def processWire(wire: List[(String, Int)], x: Int, y: Int, index: Int, plan: Map[(Int, Int, Int), Int]): Map[(Int, Int, Int), Int] = {
-    val (planNew, xNew, yNew) = processElement(wire.head._1, wire.head._2, x, y, index, plan)
+  def processWire(wire: List[(String, Int)], x: Int, y: Int, index: Int, plan: Map[(Int, Int, Int), Int], wireLength: Int): Map[(Int, Int, Int), Int] = {
+    val (planNew, xNew, yNew, lengthNew) = processElement(wire.head._1, wire.head._2, x, y, index, plan, wireLength)
     if (wire.tail == Nil) planNew
-    else processWire(wire.tail, xNew, yNew, index, planNew)
+    else processWire(wire.tail, xNew, yNew, index, planNew, lengthNew)
   }
   
   def processWires(wires: Seq[List[(String, Int)]]): Map[(Int, Int, Int), Int] = {
-    wires.zipWithIndex.map(path => processWire(path._1, 0, 0, path._2, Map())).reduce((path1, path2) => path1 ++ path2)
+    wires.zipWithIndex.map(path => processWire(path._1, 0, 0, path._2, Map(), 0)).reduce((path1, path2) => path1 ++ path2)
   }
 
-  def findCross(wires: Seq[List[(String, Int)]]) = {
-    val cross = processWires(wires)
-      .groupBy { case ((x, y, _), _) => (x, y) }
+  def findCross(plan: Map[(Int, Int, Int), Int]) = {
+    val cross = plan.groupBy { case ((x, y, _), _) => (x, y) }
       .map { case (k, v) => (k, v.size) }
       .filter { _._2 > 1 }.toList
       .map { _._1 }
@@ -49,5 +49,15 @@ object Day03 extends App {
     cross._1.abs + cross._2.abs
   }
 
-  println(s"Day 03 part1 = ${findCross(wires)}")
+  def  findCrossForShortestWires(plan: Map[(Int, Int, Int), Int]) = {
+    val cross = plan.groupBy { case ((x, y, _), _) => (x, y) }
+      .filter { _._2.size > 1 }.toList
+      .map { p => (p._1, p._2.toList.foldLeft(0)(_ + _._2)) }
+      .reduce { (p1, p2) => if (p1._2 < p2._2) p1 else p2 }
+    cross._2
+  }
+
+  val plan = processWires(wires)
+  println(s"Day 03 part1 = ${findCross(plan)}")
+  println(s"Day 03 part2 = ${findCrossForShortestWires(plan)}")
 }
--- src/test/scala/Day03Test.scala
+++ src/test/scala/Day03Test.scala
@@ -1,45 +1,56 @@
 class Day03Test extends org.scalatest.FunSuite {
   test("Day03 test processElement R1") {
-    val plan = Day03.processElement("R", 1, 0, 0, 0, Map())
-    assert(plan === (Map((1, 0, 0) -> 1), 1, 0))
+    val plan = Day03.processElement("R", 1, 0, 0, 0, Map(), 0)
+    assert(plan === (Map((1, 0, 0) -> 1), 1, 0, 1))
   }
 
   test("Day03 test processElement R2") {
-    val plan = Day03.processElement("R", 2, 0, 0, 0, Map())
-    assert(plan === (Map((1, 0, 0) -> 1, (2, 0, 0) -> 1), 2, 0))
+    val plan = Day03.processElement("R", 2, 0, 0, 0, Map(), 0)
+    assert(plan === (Map((1, 0, 0) -> 1, (2, 0, 0) -> 2), 2, 0, 2))
   }
 
   test("Day03 test processElement U2") {
-    val plan = Day03.processElement("U", 2, 0, 0, 0, Map())
-    assert(plan === (Map((0, -1, 0) -> 1, (0, -2, 0) -> 1), 0, -2))
+    val plan = Day03.processElement("U", 2, 0, 0, 0, Map(), 0)
+    assert(plan === (Map((0, -1, 0) -> 1, (0, -2, 0) -> 2), 0, -2, 2))
   }
 
   test("Day03 test processElement D2") {
-    val plan = Day03.processElement("D", 2, 0, 0, 0, Map())
-    assert(plan === (Map((0, 1, 0) -> 1, (0, 2, 0) -> 1), 0, 2))
+    val plan = Day03.processElement("D", 2, 0, 0, 0, Map(), 0)
+    assert(plan === (Map((0, 1, 0) -> 1, (0, 2, 0) -> 2), 0, 2, 2))
   }
 
   test("Day03 test processWires seq one wire") {
     val plan = Day03.processWires(Seq(List(("R", 2), ("U", 1))))
-    assert(plan === Map((1, 0, 0) -> 1, (2, 0, 0) -> 1, (2, -1, 0) -> 1))
+    assert(plan === Map((1, 0, 0) -> 1, (2, 0, 0) -> 2, (2, -1, 0) -> 3))
+  }
+
+  test("Day03 test processWires seq one wire with duplication") {
+    val plan = Day03.processWires(Seq(List(("R", 2), ("L", 1), ("U", 1))))
+    assert(plan === Map((1, 0, 0) -> 1, (2, 0, 0) -> 2, (1, -1, 0) -> 4))
   }
 
   test("Day03 test processWires seq two wires") {
     val plan = Day03.processWires(Seq(List(("R", 2), ("U", 1)), List(("D", 2))))
-    assert(plan === Map((1, 0, 0) -> 1, (2, 0, 0) -> 1, (2, -1, 0) -> 1, (0, 1, 1) -> 1, (0, 2, 1) -> 1))
+    assert(plan === Map((1, 0, 0) -> 1, (2, 0, 0) -> 2, (2, -1, 0) -> 3, (0, 1, 1) -> 1, (0, 2, 1) -> 2))
   }
 
   test("Day03 test processWires wires cross") {
     val plan = Day03.processWires(Seq(List(("R", 1), ("U", 1)), List(("U", 1), ("R", 1))))
-    assert(plan === Map((1, 0, 0) -> 1, (1, -1, 0) -> 1, (0, -1, 1) -> 1, (1, -1, 1) -> 1))
+    assert(plan === Map((1, 0, 0) -> 1, (1, -1, 0) -> 2, (0, -1, 1) -> 1, (1, -1, 1) -> 2))
   }
 
   test("Day03 test findCross") {
-    val dist = Day03.findCross(Seq(List(("R", 1), ("U", 1)), List(("U", 1), ("R", 1))))
+    val dist = Day03.findCross(Day03.processWires(Seq(List(("R", 1), ("U", 1)), List(("U", 1), ("R", 1)))))
     assert(dist === 2)
   }
 
-  test("Day03 test findCross R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83") {
+  test("Day03 test findCrossForShortestWires") {
+    val plan = Day03.processWires(Seq(List(("R", 1), ("U", 1)), List(("U", 1), ("R", 1))))
+    val dist = Day03.findCrossForShortestWires(plan)
+    assert(dist === 4)
+  }
+
+  test("Day03 test R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83") {
     val pattern = "([URLD]+)(\\d+)".r
     val wires = Seq("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")
                   .map(
@@ -49,8 +60,26 @@ class Day03Test extends org.scalatest.FunSuite {
                         (command, dist.toInt)  
                         }).toList
                     )    
-    val dist = Day03.findCross(wires)
-    assert(dist === 159)
+    val plan = Day03.processWires(wires)
+    val dist1 = Day03.findCross(plan)
+    val dist2 = Day03.findCrossForShortestWires(plan)
+    assert(dist1 === 159)
+    assert(dist2 === 610)
+  }
+
+  test("Day03 test R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 and U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") {
+    val pattern = "([URLD]+)(\\d+)".r
+    val wires = Seq("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+                  .map(
+                    _.split(",")
+                      .map(inst => {
+                        val pattern(command, dist) = inst
+                        (command, dist.toInt)  
+                        }).toList
+                    )    
+    val plan = Day03.processWires(wires)
+    val dist2 = Day03.findCrossForShortestWires(plan)
+    assert(dist2 === 410)
   }
 }
 
