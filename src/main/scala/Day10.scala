7aa5d0a74394fa4c1d64f3f771bceb044e940730
Miskpec <info@mikspec.pl>
2020-11-17 18:10:10 +0100

    Day 10 part 2

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,8 +1,11 @@
 import scala.annotation.tailrec
+import javax.security.sasl.RealmCallback
+import scala.collection.immutable.ListMap
 
 object Day10 extends App {
 
-  val image = scala.io.Source.fromResource("inputs/day10.txt").getLines().toSeq
+  val image =
+    scala.io.Source.fromResource("inputs/day10.txt").getLines().toSeq
 
   case class Position(rowNum: Int, colNum: Int)
 
@@ -45,5 +48,60 @@ object Day10 extends App {
       }
       .reduce { (a, b) => if (a._2 > b._2) a else b }
 
-  println(s"Day 10 part1 = ${processSkyMap(createMap(image))}")
+  val skyMap = createMap(image)
+  val station = processSkyMap(skyMap)
+
+  println(s"Day 10 part1 = ${station}")
+
+  def calcAngle(
+      stationPos: Position,
+      asteroid: Position
+  ): Tuple3[Double, Int, Position] = {
+    val x = asteroid.colNum - stationPos.colNum
+    val y = asteroid.rowNum - stationPos.rowNum
+    val d = Math.abs(x) + Math.abs(y)
+    val angle =
+      if (x >= 0 && y < 0) x.toDouble / d
+      else if (x >= 0 && y >= 0) 2 - x.toDouble / d
+      else if (x < 0 && y >= 0) 2 + Math.abs(x.toDouble) / d
+      else 4 - Math.abs(x.toDouble) / d
+
+    (angle, d, asteroid)
+  }
+
+  def laserMap(
+      stationPos: Position,
+      skyMap: List[Position]
+  ): ListMap[Double, List[Tuple3[Double, Int, Position]]] =
+    ListMap(
+      skyMap
+        .map { asteroid => calcAngle(stationPos, asteroid) }
+        .groupBy { case (angle, _, _) => angle }
+        .toSeq
+        .sortBy(_._1): _*
+    )
+      .map { case (k, v) =>
+        (k, v.sortBy { case (_, distance, _) => distance })
+      }
+
+  @tailrec
+  def laserRun(
+      skyMap: ListMap[Double, List[Tuple3[Double, Int, Position]]],
+      lastVaporized: Position,
+      count: Int
+  ): Position = {
+    if (count == 0) lastVaporized
+    else if (skyMap.head._2.size == 1)
+      laserRun(skyMap.tail, skyMap.head._2.head._3, count - 1)
+    else
+      laserRun(
+        skyMap.tail + ((skyMap.head._1, skyMap.head._2.tail)),
+        skyMap.head._2.head._3,
+        count - 1
+      )
+  }
+
+  val lastVaporized =
+    laserRun(laserMap(station._1, (skyMap - station._1).toList), null, 200)
+  println(s"Day 10 part 2 ${lastVaporized.colNum * 100 + lastVaporized.rowNum}")
 }
--- src/test/scala/Day10Test.scala
+++ src/test/scala/Day10Test.scala
@@ -1,15 +1,29 @@
 class Day10Test extends org.scalatest.FunSuite {
-  test("Day10 test 01") {
-    val data = Seq(
-      ".#..#",
-      ".....",
-      "#####",
-      "....#",
-      "...##"
-    )
+
+  test("Day 10 test part 01") {
+
+    val image1 =
+      scala.io.Source.fromResource("inputs/day10-01.txt").getLines().toSeq
 
     assert(
-      Day10.processSkyMap(Day10.createMap(data)) === (Day10.Position(4, 3), 8)
+      Day10.processSkyMap(Day10.createMap(image1)) === (Day10.Position(4, 3), 8)
     )
   }
+
+  test("Day 10 test part 02") {
+
+    val image2 =
+      scala.io.Source.fromResource("inputs/day10-02.txt").getLines().toSeq
+    val skyMap = Day10.createMap(image2)
+    val station = Day10.processSkyMap(skyMap)
+
+    assert(
+      Day10.laserRun(
+        Day10.laserMap(station._1, (skyMap - station._1).toList),
+        null,
+        200
+      ) === Day10.Position(2, 8)
+    )
+  }
+
 }
