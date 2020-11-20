d794e3d062f6f3bb9f5419645689cafdf41ecdfa
mikspec <marcincin@wp.pl>
2020-11-21 00:36:36 +0100

    Day 11

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -1,3 +1,7 @@
+import Day09.{State, intProg}
+
+import scala.annotation.tailrec
+
 object Day11 extends App {
 
   val progCode = scala.io.Source
@@ -10,16 +14,89 @@ object Day11 extends App {
     .map { case (value, index) => BigInt(index) -> value }
     .toMap
 
-  case class Position(x: Int, y: Int)
+  object Color extends Enumeration {
+    type Color = Value
+    val WHITE, BLACK = Value
+  }
 
-  case class Direction(dirX: Int, dirY: Int)
+  case class Position(x: Int, y: Int) {
 
-  case class Field(position: Position, color: Boolean, painted: Boolean)
+    def +(direction: Direction) = Position(this.x + direction.dirX, this.y + direction.dirY)
+  }
 
-  case class Robot(position: Position, dir: Direction)
+  sealed abstract class Direction(val dirX: Int, val dirY: Int)
 
-  val state1 = Day09.intProg(progCode, Seq(0), 0, 0)
-  println(s"Day 11 part1 = ${state1}")
-  val state2 = Day09.intProg(state1.stack, Seq(), state1.index, state1.relativeBaseOffset)
-  println(s"Day 11 part2 = ${state2}")
+  case object UP extends Direction(0, -1)
+  case object RIGHT extends Direction(1, 0)
+  case object DOWN extends Direction(0, 1)
+  case object LEFT extends Direction(-1, 0)
+
+  case class Field(color: Color.Color, painted: Boolean)
+
+  type Surface = Map[Position, Field]
+
+  case class Robot(position: Position, dir: Direction) {
+
+    def paint(color: Color.Color, surface: Surface): Surface =
+      surface.updated(this.position, Field(color, true))
+
+    def turn(goRight: Int): Direction = {
+      if (goRight == 1) {
+        this.dir match {
+          case UP => RIGHT
+          case RIGHT => DOWN
+          case DOWN => LEFT
+          case LEFT => UP
+        }
+      }
+      else {
+        this.dir match {
+          case UP => LEFT
+          case LEFT => DOWN
+          case DOWN => RIGHT
+          case RIGHT => UP
+        }
+      }
+    }
+
+    def step(turnDir: Int): Robot = {
+      val newDir = this.turn(turnDir)
+      val newPos = this.position + newDir
+      Robot(newPos, newDir)
+    }
+
+    def camera(surface: Map[Position, Field]): Color.Color =
+      surface.getOrElse(this.position, Field(Color.BLACK, false)).color
+  }
+
+  @tailrec
+  def runRobot(robot: Robot,
+               surface: Surface,
+               stack: Map[BigInt, BigInt],
+               index: BigInt,
+               relativeBaseOffset: BigInt,
+               count: Int): Surface = {
+    println(s"${count} - ${index} - ${stack(index)}")
+    val color = robot.camera(surface)
+    val conf = Seq(if (color == Color.WHITE) BigInt(1) else BigInt(0))
+    intProg(stack, conf, index, relativeBaseOffset) match {
+      case State(output1, stack1, index1, relativeBaseOffset1) => {
+        if (index1 == -1) surface
+        val paintColor = if (output1 == 1) Color.WHITE else Color.BLACK
+        val newSurface = robot.paint(paintColor, surface)
+        println(s"${count+1} - ${index} - ${stack(index)}")
+        intProg(stack1, Seq(), index1, relativeBaseOffset1) match {
+          case State(output2, stack2, index2, relativeBaseOffset2)  => {
+            if (index2 == -1) newSurface
+            val newRobot = robot.step(output2.toInt)
+            runRobot(newRobot, newSurface, stack2, index2, relativeBaseOffset2, count + 2)
+          }
+        }
+      }
+    }
+  }
+
+  val surface = runRobot(Robot(Position(0,0), UP), Map(), progCode, BigInt(0), BigInt(0), 0)
+
+  println(s"${surface}")
 }
