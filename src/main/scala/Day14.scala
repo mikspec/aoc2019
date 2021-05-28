d255677ecf044327a6db7e6d9472ce8339e4953a
Mikspec <info@mikspec.pl>
2021-05-28 18:38:04 +0200

    Day14 input parser

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -1,13 +1,30 @@
 import scala.annotation.tailrec
+import scala.util.matching.Regex
 
 object Day14 extends App {
+
+  case class Item(name: String, quantity: Int) 
+
+  val compoundPattern = "(.+)=> (\\d+) ([A-Z]+)".r
+  val itemsPattern = "(\\d+) ([A-Z]+)".r
 
   val recipe = scala.io.Source
     .fromResource("inputs/day14.txt")
     .getLines()
-    .toSeq
+    .map(row => {
+      val compoundPattern(items, quantity, name) = row
+      Item(name, quantity.toInt) -> {
+        itemsPattern
+          .findAllMatchIn(items)
+          .map(x => {
+            Item(x.group(2), x.group(1).toInt)
+          })
+          .toSeq
+      }
+    })
+    .toMap
 
   println(
-    s"Day 13 part2 ${recipe}"
+    s"Day 14 part1 ${recipe(Item("FUEL", 1))}"
   )
 }
