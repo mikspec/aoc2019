ef21cae17b1f36b2e746a89cb918a9dd144bc9a2
Mikspec <info@mikspec.pl>
2021-05-31 10:52:43 +0200

    Day 14 part 1

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -7,7 +7,7 @@ object Day14 extends App {
   case class Recipe(product: Item, components: Seq[Item])
 
   def getRecipe(file: String): Map[String, Recipe] = {
-
+    
     val compoundPattern = "(.+) => (\\d+) ([A-Z]+)".r
     val itemsPattern = "(\\d+) ([A-Z]+)".r
 
@@ -50,56 +50,64 @@ object Day14 extends App {
       )
   }
 
-  val recipe = getRecipe("inputs/day14-01.txt")
+  val recipe = getRecipe("inputs/day14.txt")
 
+  @tailrec
   def processRecipe(
       productList: Seq[Item],
       fullRecipe: Map[String, Recipe],
       accum: Map[String, Item]
   ): Map[String, Item] = {
 
-    if (productList.size == 0) return accum
-
-    val product = productList.head
-    val output = accum.getOrElse(
-      product.name,
-      Item(product.name, 0, 0)
-    )
-
-    var newAccum = accum
-
-    if (product.quantityNeeded <= (output.quantityProduced - output.quantityProduced))
-      // Enough produced ingredients
-      newAccum = accum.updated(
-        product.name,
-        Item(
-          product.name,
-          product.quantityNeeded + output.quantityNeeded,
-          output.quantityProduced
-        )
-      )
+    if (productList.size == 0) accum
     else {
-      val quantity = product.quantityNeeded - output.quantityProduced
-      val recipe = fullRecipe.getOrElse(product.name, Recipe(product, Seq()))
-      val factor =
-        (quantity / recipe.product.quantityNeeded) + (if (
-                                                        quantity % recipe.product.quantityNeeded > 0
-                                                      ) 1
-                                                      else 0)
-      val components = multiplyItems(factor, recipe.components, Seq())
-      newAccum = accum.updated(
+      val product = productList.head
+      val output = accum.getOrElse(
         product.name,
-        Item(
-          product.name,
-          output.quantityNeeded + quantity,
-          output.quantityProduced + recipe.product.quantityNeeded * factor
-        )
+        Item(product.name, 0, 0)
       )
-      newAccum = processRecipe(components, fullRecipe, newAccum)
-    }
+      var newAccum = accum
+      var components: Seq[Item] = Seq()
 
-    processRecipe(productList.tail, fullRecipe, newAccum)
+      if (
+        product.quantityNeeded <= (output.quantityProduced - output.quantityNeeded)
+      )
+        // Enough produced ingredients
+        newAccum = accum.updated(
+          product.name,
+          Item(
+            product.name,
+            product.quantityNeeded + output.quantityNeeded,
+            output.quantityProduced
+          )
+        )
+      else {
+        val quantity =
+          product.quantityNeeded - (output.quantityProduced - output.quantityNeeded)
+        val recipe = fullRecipe.getOrElse(product.name, Recipe(product, Seq()))
+        val factor =
+          (quantity / recipe.product.quantityNeeded) + (if (
+                                                          quantity % recipe.product.quantityNeeded > 0
+                                                        ) 1
+                                                        else 0)
+        components = multiplyItems(factor, recipe.components, Seq())
+        newAccum = accum.updated(
+          product.name,
+          Item(
+            product.name,
+            output.quantityNeeded + product.quantityNeeded,
+            output.quantityProduced + recipe.product.quantityNeeded * factor
+          )
+        )
+      }
+
+      processRecipe(components ++ productList.tail, fullRecipe, newAccum)
+    }
   }
 
-  println(s"Day 14 part1 ${processRecipe(Seq(Item("FUEL", 1, 0)), recipe, Map())}")
+  val oreNeeded = processRecipe(Seq(Item("FUEL", 1, 0)), recipe, Map())("ORE").quantityNeeded
+
+  println(
+    s"Day 14 part1 ${oreNeeded}"
+  )
 }
--- src/test/scala/Day14Test.scala
+++ src/test/scala/Day14Test.scala
@@ -1,10 +1,22 @@
-import Day14.{getRecipe}
+import Day14.{getRecipe, processRecipe, Item}
 
 class Day14Test extends org.scalatest.FunSuite {
 
-  test("Day 14 test part 01") {
+  test("Day 14 test part 01 - case 01") {
 
     val recipe = getRecipe("inputs/day14-01.txt")
+    assert(processRecipe(Seq(Item("FUEL", 1, 0)), recipe, Map())("ORE").quantityNeeded === 31)
   }
 
+  test("Day 14 test part 01 - case 02") {
+
+    val recipe = getRecipe("inputs/day14-02.txt")
+    assert(processRecipe(Seq(Item("FUEL", 1, 0)), recipe, Map())("ORE").quantityNeeded === 165)
+  }
+
+  test("Day 14 test part 01 - case 03") {
+
+    val recipe = getRecipe("inputs/day14-03.txt")
+    assert(processRecipe(Seq(Item("FUEL", 1, 0)), recipe, Map())("ORE").quantityNeeded === 13312)
+  }
 }
