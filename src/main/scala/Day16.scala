4595777b5722e9483fcf808cca3608a037799944
Mikspec <info@mikspec.pl>
2021-06-07 15:02:24 +0200

    Day 16 Initial commit

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -1,16 +1,46 @@
 import scala.annotation.tailrec
-import scala.util.matching.Regex
-
 object Day16 extends App {
 
-  val PATTERN:List[Int] = List(0, 1, 0, -1)
+  val PATTERN: List[Int] = List(0, 1, 0, -1)
 
-  def getInput(file: String):List[Int] = {
+  def getInput(file: String): List[Int] = {
     scala.io.Source
       .fromResource(file)
       .toList
       .map(digit => digit - '0'.toInt)
-  }  
+  }
 
-  println(s"Day 16 part 1 ${getInput("inputs/day16.txt")}")
+  @tailrec
+  def multiplyPattern(
+      m: Int,
+      sourcePattern: List[Int],
+      accumCounter: Int,
+      accumList: List[Int]
+  ): List[Int] = {
+    if (sourcePattern.isEmpty) accumList
+    else {
+      if (accumCounter > 0)
+        multiplyPattern(
+          m,
+          sourcePattern,
+          accumCounter - 1,
+          accumList :+ sourcePattern.head
+        )
+      else multiplyPattern(m, sourcePattern.tail, m, accumList)
+    }
+  }
+
+  def generatePattern(
+      n: Int,
+      sourcePattern: List[Int]
+  ): LazyList[Int] = {
+
+    val pattern = multiplyPattern(n, sourcePattern, n, List()).toVector
+
+    def loop(n: Int, position: Int): LazyList[Int] =
+      pattern(position % pattern.size) #:: loop(n, position + 1)
+    loop(n, 0)
+  }
+
+  println(s"Day 16 part 1 ${generatePattern(3, PATTERN).drop(1).take(650).toList}")
 }
